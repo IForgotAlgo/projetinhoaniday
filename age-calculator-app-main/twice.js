08f83bde363597755b00958347f9126270c8269b
= <44711593+IForgotAlgo@users.noreply.github.com>
2023-09-02 23:36:20 -0300

    Tags de Inputs corrigidas para inicio code em JS.

---
+++ age-calculator-app-main/twice.js
@@ -0,0 +1,9 @@
+function apertar_botao() {
+    let dias = document.getElementById("dia").value;
+    let mes = document.getElementById("mes").value;
+    let ano = document.getElementById("ano").value;
+    let data = new Date();
+
+    console.log(data);
+
+}
