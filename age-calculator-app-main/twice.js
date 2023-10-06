9a5b49bd66e2d84a13cdab2c0d86f8e3cca46383
= <44711593+IForgotAlgo@users.noreply.github.com>
2023-10-06 11:21:27 -0300

    backup

--- age-calculator-app-main/twice.js
+++ age-calculator-app-main/twice.js
@@ -2,8 +2,15 @@ function apertar_botao() {
     let dias = document.getElementById("dia").value;
     let mes = document.getElementById("mes").value;
     let ano = document.getElementById("ano").value;
+
     let data = new Date();
 
-    console.log(data);
+    data = data.getFullYear();
+    
+    /* Convertendo para Inteiro e subtraindo o valor do ano informado pelo usuário*/
+    let idade = parseInt(data) - ano;
+
+    console.log(idade);       
+
 
 }
